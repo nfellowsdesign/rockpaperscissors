afc7729c3ba4b007456b54f0c88b4d52d55da758
Nathan Fellows <nfellowsdesign@gmail.com>
2021-10-13 21:00:59 -0700

    added basic gameRound function w/ computerPlay and userPlay also added username prompt

--- scripts.js
+++ scripts.js
@@ -1,2 +1,21 @@
 
+//username input
 
+let username = prompt("Enter your Name")
+
+//game aspects
+function gameRound(playerSelection,computerSelection) {
+    function computerPlay(){
+       let computerSelection = "rock"
+
+    }
+    function userPlay(){
+        let userSelection = prompt("Enter your Move");
+    }
+    computerPlay();
+    userPlay();
+}
+
+gameRound()
+
+//scorekeeping
