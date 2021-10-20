7026571657c319424ad4ff13625ad17a4ec10e7f
Nathan Fellows <nfellowsdesign@gmail.com>
2021-10-19 22:47:20 -0700

    reordered functions to make it clearer/ cleaned up some small parts

--- scripts.js
+++ scripts.js
@@ -1,7 +1,38 @@
+//initial states
+let userScore = 0;
+let computerScore = 0;
+let roundTracker = 1;
+console.log("Round:"+roundTracker);
 
+//calling ui elements
 const rockBtn = document.querySelector("#rock")
 const paperBtn = document.querySelector("#paper")
 const scissorsBtn = document.querySelector("#scissors")
+
+//Play Button Events
+rockBtn.addEventListener('click', function(e){
+    let userSelection = 'rock';
+    let computerSelection = computerPlay();
+    let roundResult = playRound(userSelection,computerSelection);
+    console.log(roundResult);
+    game(roundResult);
+});
+   
+paperBtn.addEventListener('click', function(e){
+    let userSelection = 'paper';
+    let computerSelection = computerPlay();
+    let roundResult = playRound(userSelection,computerSelection);
+    console.log(roundResult);
+    game(roundResult);
+});
+
+scissorsBtn.addEventListener('click', function(e){
+    let userSelection = 'scissors';
+    let computerSelection = computerPlay();
+    let roundResult = playRound(userSelection,computerSelection);
+    console.log(roundResult);
+    game(roundResult);
+});
 
 //computer move function
 function computerPlay(){
@@ -9,23 +40,20 @@ function computerPlay(){
     switch (roll) {
         case 1: {
             return "rock";
-           // console.log("Computer Move:"+computerSelection);
-        break;
+            break;
         }
         case 2: {
            return "paper";
-           //console.log("Computer Move:"+computerSelection);
-        break;
+            break;
         }
         case 3: {
             return "scissors";
-           //console.log("Computer Move:"+computerSelection);
-        break;
+            break;
         }
     }
 }
 
-//game round function
+//individual round function
 function playRound(userSelection, computerSelection) {
     //Results if user picks rock
     if (userSelection == "rock"){
@@ -59,65 +87,35 @@ function playRound(userSelection, computerSelection) {
     }
 }
 
-rockBtn.addEventListener('click', function(e){
-    let userSelection = 'rock';
-    let computerSelection = computerPlay();
-    let roundResult = playRound(userSelection,computerSelection);
-    console.log(roundResult);
-    game(roundResult);
-});
-   
-paperBtn.addEventListener('click', function(e){
-    let userSelection = 'paper';
-    let computerSelection = computerPlay();
-    let roundResult = playRound(userSelection,computerSelection);
-    console.log(roundResult);
-    game(roundResult);
-});
-
-scissorsBtn.addEventListener('click', function(e){
-    let userSelection = 'scissors';
-    let computerSelection = computerPlay();
-    let roundResult = playRound(userSelection,computerSelection);
-    console.log(roundResult);
-    game(roundResult);
-});
-
-
-//initial states
-let userScore = 0;
-let computerScore = 0;
-let roundTracker = 1;
-console.log("Round:"+roundTracker);
-
+//game function
 function game(roundResult){
-        switch(roundResult) {
-            case "win":{
-                roundTracker++;
-                console.log("Round:"+roundTracker);
-                userScore++;
-                console.log(userScore);
-                console.log(computerScore);
-                console.log("Round was won");
-                break;
-            }
-            case "lose":{
-                roundTracker++;
-                console.log("Round:"+roundTracker);
-                computerScore++;
-                console.log(userScore);
-                console.log(computerScore);
-                console.log("Round was lost");
-                break;
-            }
-            default:{
-                roundTracker++;
-                console.log("Round:"+roundTracker);
-                console.log(userScore);
-                console.log(computerScore);
-                console.log("Round was tied");
-            }
+    switch(roundResult) {
+        case "win":{
+            roundTracker++;
+            console.log("Round:"+roundTracker);
+            userScore++;
+            console.log(userScore);
+            console.log(computerScore);
+            console.log("Round was won");
+            break;
         }
+        case "lose":{
+            roundTracker++;
+            console.log("Round:"+roundTracker);
+            computerScore++;
+            console.log(userScore);
+            console.log(computerScore);
+            console.log("Round was lost");
+            break;
+        }
+        default:{
+            roundTracker++;
+            console.log("Round:"+roundTracker);
+            console.log(userScore);
+            console.log(computerScore);
+            console.log("Round was tied");
+        }
+    }
         if (userScore == 5){
             console.log("Game over: Player Wins");
         } 
